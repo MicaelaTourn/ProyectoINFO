e4be79fa241bb7a77efe8faad589dff046b681f7
Gerardo-Rioss <gerarios26@gmail.com>
2023-07-26 18:11:33 -0300

    Tipo_usuario

--- apps/usuarios/admin.py
+++ apps/usuarios/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
-admin.site.register(Usuario)
-# Register your models here.
+class UsuarioAdmin(UserAdmin):
+    model = Usuario
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'tipo_usuario']
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('imagen', 'tipo_usuario')}),
+    )
+
+admin.site.register(Usuario, UsuarioAdmin)
--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -1,10 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Usuario(AbstractUser):
     imagen = models.ImageField(upload_to='usuarios', default='default-user.png')
-    tipo_usuario = models.CharField(max_length=11, choices=[('publico', 'Publico'), ('colaborador', 'Colaborador')], default='publico')
-    pass
+    
 
-# Create your models here.
+    # USUARIOS:
+    USUARIO_COLABORADOR = 'Colaborador'
+    USUARIO_VISITANTE = 'Visitante'
+    USUARIO_MIEMBRO = 'Miembro'
+    USUARIO_SUPER = 'Superusuario'
 
+    TIPO_DE_USUARIO=[
+        (USUARIO_COLABORADOR, 'Colaborador'), # is_superuser = False, is_staff = True
+        (USUARIO_VISITANTE,'Visitante'), # no esta en la base de datos
+        (USUARIO_MIEMBRO,'Miembro'), # is_superuser = False, is_staff = False
+        (USUARIO_SUPER,'Superusuario'), # is_superuser = True, is_staff = True
+    ]
+
+    tipo_usuario = models.CharField(max_length=20, choices=TIPO_DE_USUARIO, default=USUARIO_MIEMBRO )
+
+    def __str__(self):
+        return self.username
+
+# Señal para asignar el tipo de usuario "Superusuario" cuando se crea un superusuario
+@receiver(post_save, sender=Usuario)
+def asignar_tipo_usuario(sender, instance, created, **kwargs):
+    if created and instance.is_superuser:
+        instance.tipo_usuario = Usuario.USUARIO_SUPER
+        instance.save()
+
+# Señal para asignar el tipo de usuario "Miembro" cuando se crea un usuario
+@receiver(post_save, sender=Usuario)
+def asignar_miembro(sender, instance, created, **kwargs):
+    if created and instance.is_superuser:
+        instance.tipo_usuario= Usuario.USUARIO_MIEMBRO
+        instance.save()
