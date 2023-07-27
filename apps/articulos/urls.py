a36d4edb26f6f0fd9e4cfc1d214f118312a4d3a2
Gerardo-Rioss <gerarios26@gmail.com>
2023-07-27 19:14:13 -0300

    Corregido carga de articulos

--- apps/articulos/urls.py
+++ apps/articulos/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('comentario/add/<int:articulo_id>/', views.add_comentario, name='add_comentario'),
     path('comentario/edit/<int:comentario_id>/', views.edit_comentario, name='edit_comentario'),
     path('comentario/delete/<int:comentario_id>/', views.delete_comentario, name='delete_comentario'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
