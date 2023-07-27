705013e075a9efedf8368eb83ca4567d526e9391
Aguirre Mauricio <mauricioaguirre.elec@gmail.com>
2023-07-27 17:17:47 -0300

    Ya funciona el envío de correos electrónicos

--- apps/contacto/views.py
+++ apps/contacto/views.py
@@ -4,16 +4,37 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from .forms import ContactForm
 from django.contrib import messages
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
+
+        
         if form.is_valid():
             # Process the form data
             nuevoContacto = form.save(commit=False)
+            nombreRemitente = request.POST.get('nombre')
+            apellidoRemitente = request.POST.get('apellido')
+            mensajeEnviado = request.POST.get('mensaje')
+            emailRemitente = request.POST.get('email')
+            
+            data = {
+                'nombre': nombreRemitente,
+                'apellido': apellidoRemitente,
+                'email': emailRemitente,
+                'mensaje': mensajeEnviado
+            }
+            message = '''
+            NuevoMensaje: {}
+
+
+            From: {}
+            '''.format(data['mensaje'],data['email'])
             nuevoContacto.save()
+            send_mail(f'Contacto desde la web: {nombreRemitente} {apellidoRemitente}', message, '',['blogcocinagrupo4@gmail.com'])
             messages.add_message(request,level=messages.SUCCESS, message='Formulario enviado!')
             return redirect('home')
         else:
--- blog_cocina/settings/local.py
+++ blog_cocina/settings/local.py
@@ -4,7 +4,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+EMAIL_HOST_USER = '7b532dc35268d6'
+EMAIL_HOST_PASSWORD = 'd807a5320a56b8'
+EMAIL_PORT = 587
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
